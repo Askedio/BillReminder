fddd5c20750da8f60cb48698b853ad1f3b2f2905
william bowman <gcphost@gmail.com>
2016-03-03 12:03:31 -0800

    updated some copy, sure need design and copywriters :(

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -11,13 +11,20 @@
   @endif
 
   <div class="jumbotron">
-    <h1>BillReminder <sup style="font-size: 20px"> Beta</sup></h1>
+    <h1>Bill Reminder <sup style="font-size: 20px"> Beta</sup></h1>
     <p>A Google Calendar based Bill Reminder service.</p>
     <hr>
 
     @if (Auth::guest())
-      <p><small>You will need to allow <strong>BillReminder Beta</strong> access to your Google Calendar in order for it to manage your calendars events. Click the button below to grant access, create an account here and login.</small></p>
-      <a class="btn btn-warning btn-lg" href="{{ url('/auth') }}"><em class="fa fa-key"></em> Authenticate with Google</a>
+      <p><small>Bill Reminder uses Google Calender to manage your bills as Events. We store a little bit of data in the description and parse it out so you can see what bills are due, when and how much. <a href="{{ url('/auth') }}">Login with Google</a> to get started!</small></p>
+
+      <p>
+        <a href="{{ url('/auth') }}"><img class="img-responsive" src="https://camo.githubusercontent.com/8346bc72856242f677bac4bc5cce851bfec4a502/687474703a2f2f692e696d6775722e636f6d2f537a4965316e6f2e706e67"></a>
+      </p>
+
+      <p class="text-center">
+        <a class="btn btn-warning btn-lg" href="{{ url('/auth') }}"><em class="fa fa-key"></em> Authenticate with Google</a>
+      </p>
 
     @else
       <h2>Welcome back, {{ Auth::user()->name }}!</h2>
@@ -30,6 +37,5 @@
     @endif
   </div>
   <div class="alert alert-warning text-center"><h4>We save your name, email, avatar, reminder calendar, and authentication token from Google.</h4></div>
-  <div class="alert alert-info text-center"><h4>BillReminder is a free service and is using a free Google API key that has limits!</h4></div>
 </section>
 @endsection
